749a1ce9c70b18f94235ad9736cfaf6a650e5be8
Chandan Sen <senchandan939@gmail.com>
2020-01-13 00:52:54 +0530

    clint list changes

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -4,13 +4,12 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class NewClientService {
+export class ClientService {
 
   constructor(private apiService: ApiService, private http: HttpClient) { }
 
-  public addClient(UserDetail: any){
-    const url = this.apiService.getUrl("register");
-    // console.log(url);
-    return this.http.post(url, UserDetail);
+  getClientList(){
+    const url = this.apiService.getUrl("userClientList");
+    return this.http.get(url);
   }
 }
